f7f706a75f9ddfa538b88c7dab6163196a36ed27
HeerRik <rik.bandsma@gmail.com>
2016-04-06 15:54:00 +0200

    Save and store functionality

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# WIP poolbuilder
-Application for creating champ select filters for the game League of Legends, written in Python.
+# Poolbuilder Alpha
+Poolbuilder is an application for creating champ select filters for the game League of Legends.
 
 This is a project to get to know Python, as well as give a little something to the League of Legends community.
 Questions about the software, aswell as suggestions, are welcome.
+
+If you encounter any show stopping bugs, please let me know.
+Bugs that result from using the software in an incorrect way, may or may not be solved some day.
+A known problem is that champions inside a loaded pool still appear in the champions window, this will
+be dealt with.
+
+“"Python" is a registered trademark of the Python Software Foundation”
--- poolbuilder.py
+++ poolbuilder.py
@@ -1,6 +1,13 @@
 from ttk import Frame, Label, Button, Style
 from Tkinter import *
+import tkSimpleDialog
 
+"""
+Poolbuilder v0.6
+
+Author: Rik Bandsma
+Github: https://github.com/HeerRik
+"""
 
 class App(Frame):
 
@@ -20,6 +27,7 @@ class App(Frame):
         self.pack(fill=BOTH, expand=1)
 
         self.buildMode = True
+        self.allPools = "assets/pools/"
 
         self.champsLabel = Label(text="Champions")
         self.champBox = Listbox(self)
@@ -32,6 +40,7 @@ class App(Frame):
         self.saveButton = Button(self, text="Save champion pool", width=18, command=self.saveChampPool)
         self.loadButton = Button(self, text="Load champion pool", width=18, command=lambda: self.loadChampPools(1))
         self.confirmLoadButton = Button(self, text="Load", width=6, command=self.choosePool)
+        self.getStringButton = Button(self, text="Copy pool to clipboard", width=18, command=self.buildPoolString)
 
         self.poolLabel = Label(text="Champion Pool")
         self.poolBox = Listbox(self)
@@ -46,6 +55,7 @@ class App(Frame):
         self.removeButton.place(x=150, y=100)
         self.saveButton.place(x=350, y=30)
         self.loadButton.place(x=350, y=60)
+        self.getStringButton.place(x=350, y=90)
 
         self.poolLabel.place(x=200, y=5)
         self.poolBox.place(x=200, y=30)
@@ -60,15 +70,13 @@ class App(Frame):
         sh = self.parent.winfo_screenheight()
         x = (sw - w)/2
         y = (sh - h)/2
-        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.parent.geometry("%dx%d+%d+%d" % (w, h, x, y))
 
     def loadChampions(self):
-        with open('assets/Champions.txt', 'r') as file:
+        with open("assets/Champions.txt", "r") as file:
             champions = file.read().splitlines()
         for champion in sorted(champions):
             self.champBox.insert(END, champion)
-        file.close()
-
     def choosePool(self):
         idx = self.poolBox.curselection()
         chosenPool = self.poolBox.get(idx)
@@ -79,25 +87,29 @@ class App(Frame):
         if level == 1:
             self.buildMode = False
             self.champBox.delete(0, END)
-            with open('assets/champpools.txt', 'r') as file:
-                champPools = file.read().splitlines()
+            self.poolBox.delete(0, END)
             self.saveButton.config(state=DISABLED)
             self.loadButton.config(state=DISABLED)
+            self.getStringButton.config(state=DISABLED)
+            self.confirmLoadButton.place(x=350, y=120)
+
+            with open("assets/champpools.txt", "r") as file:
+                champPools = file.read().splitlines()
             for pool in champPools:
                 self.poolBox.insert(END, pool)
-            self.confirmLoadButton.place(x=350, y=90)
         elif level == 2:
-            print "level 2"
             self.poolBox.delete(0, END)
             self.buildMode = True
             self.loadButton.config(state=NORMAL)
             self.saveButton.config(state=NORMAL)
-            fileName = pool + '.txt'
-            with open('assets/' + fileName, 'r') as file:
+            self.getStringButton.config(state=NORMAL)
+            self.loadChampions()
+
+            fileName = pool + ".txt"
+            with open(self.allPools + fileName, "r") as file:
                 champPool = file.read().splitlines()
             for champion in sorted(champPool):
                 self.poolBox.insert(END, champion)
-            self.loadChampions()
 
     def addChampToPool(self):
         idx = self.champBox.curselection()
@@ -115,8 +127,43 @@ class App(Frame):
 
     def saveChampPool(self):
         championPool = self.poolBox.get(0, END)
-        for champion in championPool:
-            print champion
+        poolTitle = TitleDialog(self.parent)
+        fileName = self.allPools + poolTitle.title + ".txt"
+        with open(fileName, "w+") as file:
+            for champion in sorted(championPool):
+                file.write(champion + "\n")
+        with open("assets/champpools.txt", "a")as file:
+            file.write(poolTitle.title)
+
+    def buildPoolString(self):
+        clipper = Tk()
+        clipper.withdraw()
+        clipper.clipboard_clear()
+
+        championPool = self.poolBox.get(0, END)
+
+        if championPool:
+            championPoolString = ""
+            for champion in championPool:
+                championPoolString += "^" + champion + "$|"
+
+            clipper.clipboard_append(championPoolString)
+            clipper.destroy()
+
+class TitleDialog(tkSimpleDialog.Dialog):
+
+    def body(self, parent):
+        self.parent = parent
+
+        questionLabel = Label(self.parent, text="Give your champion pool a name:")
+        questionLabel.grid(row=0)
+        self.poolTitle = Entry(self.parent)
+        self.poolTitle.grid(row=1)
+        return self.poolTitle
+
+    def apply(self):
+        title = self.poolTitle.get()
+        self.title = title
 
 def main():
     root = Tk()
@@ -124,6 +171,5 @@ def main():
     ex = App(root)
     root.mainloop()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
